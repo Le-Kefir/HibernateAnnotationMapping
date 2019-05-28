618d56f698dbea485d4a0e8068b748ebed365f07
Darkhan Bailov <darkhan@Darkhans-MacBook-Pro.local>
2019-05-28 12:29:49 +0200

    final

---
+++ src/main/java/domain/Domain.java
@@ -0,0 +1,55 @@
+package domain;
+
+import bl.HibernateUtil;
+import entity.Address;
+import entity.Employee;
+import entity.Project;
+import org.hibernate.Session;
+
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Domain {
+
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Address address = new Address();
+        address.setCountry("USA");
+        address.setCity("NY");
+        address.setStreet("Liberty st.");
+        address.setPostCode("10032");
+
+        Employee employee = new Employee();
+        employee.setFirstName("John");
+        employee.setLastName("Doe");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1996, Calendar.MARCH, 7);
+
+        employee.setBirthday(new Date(calendar.getTime().getTime()));
+        employee.setAddress(address);
+
+        Project project = new Project();
+        project.setTitle("New Life");
+
+        Set<Project> projects = new HashSet<Project>();
+        projects.add(project);
+        employee.setProjects(projects);
+
+        session.save(address);
+        session.save(employee);
+        session.save(project);
+
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
+
+    }
+
+
+}
